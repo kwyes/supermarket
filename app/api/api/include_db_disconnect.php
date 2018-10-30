fd0d1b98f19f2d4172886ba8ddee5c8cfdd4a7c9
Chano <kwyes2@hotmail.com>
2018-10-30 00:41:37 -0700

    remove webstom {folder name} directory

--- app/api/api/include_db_disconnect.php
+++
@@ -1,9 +0,0 @@
-<?
-	
-
-	// Close mssql connection
-	mssql_close($conn_bby);
-	mssql_close($conn_sry);
-	mssql_close($conn_hannam);
-	mssql_close($conn_office);
-?>
--- app/api/api/index.php
+++
@@ -1,5 +0,0 @@
-<?php
-
-
-	defined('_APP_PHPMODULE') or die('Access Error.');
-?>
